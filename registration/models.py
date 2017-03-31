fb6998d931976354b69576d752d43ff55d9bc01d
okkhoury <okk6nb@virginia.edu>
2017-03-31 00:27:19 -0400

    changed something with the report inputs. I'll add the rest of them soon.

--- registration/models.py
+++ registration/models.py
@@ -18,14 +18,14 @@ class SiteUser(AbstractUser):
 class report(models.Model):
     company_name = models.CharField(max_length=50)
     company_phone = models.CharField(max_length=25)
-    ceo = models.CharField(max_length=25)
-    company_email = models.CharField(max_length=25)
-    company_location = models.CharField(max_length=25)
-    company_country = models.CharField(max_length=25)
-    company_sector = models.CharField(max_length=25)
-    company_industry = models.CharField(max_length=25)
-    company_projects = models.CharField(max_length=25)
-    company_ctp = models.CharField(max_length=25)
+    # ceo = models.CharField(max_length=25)
+    # company_email = models.CharField(max_length=25)
+    # company_location = models.CharField(max_length=25)
+    # company_country = models.CharField(max_length=25)
+    # company_sector = models.CharField(max_length=25)
+    # company_industry = models.CharField(max_length=25)
+    # company_projects = models.CharField(max_length=25)
+    # company_ctp = models.CharField(max_length=25)
 
     class Meta:
     	permissions = (
