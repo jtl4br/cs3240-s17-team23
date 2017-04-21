a796a688923c48c12b74a3d460aacbabce3d3552
jtl4br <jtl4br@virginia.edu>
2017-04-20 20:46:23 -0400

    removed validator from migrations

--- registration/migrations/0001_initial.py
+++ registration/migrations/0001_initial.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 import django.contrib.auth.models
-import django.contrib.auth.validators
+#import django.contrib.auth.validators
 from django.db import migrations, models
 import django.utils.timezone
 
