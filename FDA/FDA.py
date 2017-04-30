3e9f76f9959531be87d0b9fcc84002fb04c2a8a7
okkhoury <okk6nb@virginia.edu>
2017-04-30 16:54:57 -0400

    added basic interface to FDA and the ability to view reports

--- FDA/FDA.py
+++ FDA/FDA.py
@@ -2,8 +2,21 @@ import requests
 import json
 
 def viewReports():
-	r = requests.post('http://127.0.0.1:8000/view_reports_FDA/')
+	r = requests.post('http://127.0.0.1:8000/viewReports_FDA/')
 
+	data = r.json()
+
+	for key in data:
+		print("company name: ", data[key][0])
+		print("company phone: ", data[key][1])
+		print("ceo: ", data[key][2])
+		print("email: ", data[key][3])
+		print("location: ", data[key][4])
+		print("sector: ", data[key][5])
+		print("industry: ", data[key][6])
+		print("projects: ", data[key][7])
+		print()
+	
 	return r.json()
 
 
@@ -19,7 +32,22 @@ def login():
 	return r.get('passed')
 
 
+
+# BEGINNING OF THE USER INTERFACE FOR FDA
 if login() == 'y':
 	print("successful login")
+	print()
+	print("Welcome to Lohaki!")
+	print()
+
+	value = input("Enter '1' to view reports: ")
+
+	print(value)
+
+	if value == '1':
+		viewReports()
+
+
+
 else:
 	print("login failed")
--- FDA/views.py
+++ FDA/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.views.generic.edit import FormView
 from django.views.decorators.csrf import csrf_exempt
+from registration.models import report
 
 
 ### FUNCTIONS FOR INTERACTING WITH THE FDA ###
@@ -15,7 +16,6 @@ from django.http import HttpResponse
 @csrf_exempt
 def login_view_FDA(request):
    
-
     username = request.POST.get('username')
     password = request.POST.get('password')
 
@@ -36,27 +36,47 @@ def login_view_FDA(request):
         }
         return JsonResponse(data, safe=False)
         
-
-    
-
-# def login_view_FDA(request):
-#     context = {}
-
-#     print("HERE 1")
-
-#     if request.method == 'POST':
-
-#         print("HERE")
-
-#         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-#         if user is not None:
-#             #if user.user_type != None:
-#             data = {"passed": "y"}
-           
-#             return JsonResponse(data)
-#         else:
-#             data = {"passed": "y"}
-#             return JsonResponse(data)
-
+@csrf_exempt
 def viewReports_FDA(request):
-    return JsonResponse({'foo':'bar'}, safe=False)
+
+    reports = report.objects.all()
+
+    for rep in reports:
+        print(rep.company_name)
+
+    # Dictionary of report ids to the report attributes
+    reportDictionary = {}
+
+    for rep in reports:
+        reportDictionary[rep.id] = list()
+        reportDictionary[rep.id].append(rep.company_name)
+        reportDictionary[rep.id].append(rep.company_phone)
+        reportDictionary[rep.id].append(rep.ceo)
+        reportDictionary[rep.id].append(rep.company_email)
+        reportDictionary[rep.id].append(rep.company_location)
+        #reportDictionary[rep.id].append(rep.company_country)
+        reportDictionary[rep.id].append(rep.company_sector)
+        reportDictionary[rep.id].append(rep.company_industry)
+        reportDictionary[rep.id].append(rep.company_projects)
+
+
+    print(reportDictionary)
+
+    return JsonResponse(reportDictionary, safe=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
